91e25e208ae6d4becf3a5100e9dfe0c41e435890
mohamed3moustir <mohamedmoustir99@gmail.com>
2024-09-04 14:53:40 +0200

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -1,7 +1,12 @@
-int T[]={1,2,3,4,5};
+
+   #include <stdio.h>
+
+int main() {
+ int T[]={1,2,3,4,5};
    printf("%d\n",T[0]);
    printf("%d\n",T[1]);
    printf("%d\n",T[2]);
    printf("%d\n",T[3]);
    printf("%d\n",T[4]);
-   
+  return 0;
+}
