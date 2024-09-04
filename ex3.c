f0556a373d84eb2941a3dc012823993a74bae72a
mohamed3moustir <mohamedmoustir99@gmail.com>
2024-09-04 13:46:34 +0200

    Create ex3.c

---
+++ ex3.c
@@ -0,0 +1,11 @@
+ int M[5];
+  int max=0;
+   for(int i=0;i<5;i++){
+       printf("enter ");
+       scanf("%d",&M[i]);
+      if(max<M[i]) 
+       max=M[i];
+       
+   }
+   printf("MAX IS:%d \n",max);
+ 
